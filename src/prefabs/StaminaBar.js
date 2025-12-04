9b7700b586d11de80c6b5d7f3dc38bd7e11c600f
riyer4 <riyer4@ucsc.edu>
2025-12-04 03:58:51 -0800

    added flashing stamina bar for when it gets lower

--- src/prefabs/StaminaBar.js
+++ src/prefabs/StaminaBar.js
@@ -12,6 +12,8 @@ class StaminaBar extends Phaser.GameObjects.GameObject {
         this.staminaBar = scene.add.rectangle(0, 0, 158, 18, 0x00ff00).setScrollFactor(0).setOrigin(1, 0);
         this.staminaBarBG.setPosition(scene.cameras.main.width - 10, 10);
         this.staminaBar.setPosition(scene.cameras.main.width - 11, 11);
+
+        this.isFlashing = false;
     }
 
     update() {
@@ -21,6 +23,27 @@ class StaminaBar extends Phaser.GameObjects.GameObject {
 
         if (this.stamina <= 0) {
             this.scene.endGame();
+        }
+
+        if (this.stamina <= 25) {
+            this.staminaBar.fillColor = 0xff0000;
+            if (!this.isFlashing) {
+                this.isFlashing = true;
+                this.scene.tweens.add({
+                    targets: this.staminaBar,
+                    alpha: 0.2,
+                    duration: 200,
+                    yoyo: true,
+                    repeat: -1
+                });
+            }
+        } else {
+            this.staminaBar.fillColor = 0x00ff00;
+            if (this.isFlashing) {
+                this.isFlashing = false;
+                this.scene.tweens.killTweensOf(this.staminaBar);
+                this.staminaBar.alpha = 1;
+            }
         }
     }
 
